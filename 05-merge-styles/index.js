be691e978d8f37db4a4931c96be27f286c13707a
KaterinaMysl <KaterinaMysl>
2023-05-08 18:32:27 +0300

    feat: change result task 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -8,15 +8,12 @@ fs.readdir(stylesPath, { withFileTypes: true }, (err, files) => {
 
   const cssFiles = files.filter(file => path.extname(file.name) === '.css');
 
-  if (!fs.existsSync(path.join(__dirname, 'project-dist'))) {
-    fs.mkdirSync(path.join(__dirname, 'project-dist'));
-  }
   const writeStream = fs.createWriteStream(bundlePath);
 
   cssFiles.forEach((cssFile) => {
     const cssPath = path.join(stylesPath, cssFile.name);
     const readStream = fs.createReadStream(cssPath);
-    readStream.on('data', data => writeStream.write(data));
+    readStream.on('data', data => writeStream.write(data + '\n'));
     readStream.on('error', error => process.stdout.write('Error', error.message));
 
   });
